d478b98bd29154cabd19ff056e0f4a364a4195d8
Oswaldo Martinez Guillen <osmargui.2k1@gmail.com>
2024-04-18 13:34:20 -0600

    Installed emotion

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+/** @jsx jsx */
+import { jsx, css } from '@emotion/react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div css={css`
+      background-color: red;
+    `}>
+      test
     </div>
   );
 }
